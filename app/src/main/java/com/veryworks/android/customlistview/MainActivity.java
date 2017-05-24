e2d8f2f55778accf9742c4bee79d29ea088420c1
javafa <nigne2016@gmail.com>
2017-05-24 12:04:19 +0900

    init

--- app/src/main/java/com/veryworks/android/customlistview/MainActivity.java
+++ app/src/main/java/com/veryworks/android/customlistview/MainActivity.java
@@ -1,10 +1,14 @@
 package com.veryworks.android.customlistview;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,30 +22,67 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Data> datas = Loader.getData();
 
         // 2. 아답터
+        CustomAdapter adapter = new CustomAdapter(datas, this);
+
+        // 3. 연결
+        ((ListView) findViewById(R.id.listView)).setAdapter(adapter);
 
     }
 }
 
 class CustomAdapter extends BaseAdapter {
-    
+    // 1. 데이터
+    ArrayList<Data> datas;
+    // 2. 인플레이터
+    LayoutInflater inflater;
+
+    public CustomAdapter(ArrayList<Data> datas, Context context) {
+        this.datas = datas;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return datas.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return datas.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        // 1. 컨버터뷰를 체크해서 null 일경우만 item layout 을 생성해준다
+        Holder holder;
+        if(convertView == null) {
+            convertView = inflater.inflate(R.layout.item_list, null);
+            holder = new Holder();
+            holder.no = (TextView) convertView.findViewById(R.id.txtNo);
+            holder.title = (TextView) convertView.findViewById(R.id.txtTitle);
+
+            convertView.setTag(holder);
+        }else{
+            holder = (Holder) convertView.getTag();
+        }
+        // 2. 내 아이템에 해당하는 데이터를 세팅해준다.
+        Data data = datas.get(position);
+//        ((TextView) convertView.findViewById(R.id.txtNo)).setText(data.no + "");
+//        ((TextView) convertView.findViewById(R.id.txtTitle)).setText(data.title);
+        holder.no.setText(data.no+"");
+        holder.title.setText(data.title);
+
+        return convertView;
+    }
+
+    class Holder {
+        TextView no;
+        TextView title;
     }
 }
 
@@ -53,6 +94,7 @@ class Loader {
             Data data = new Data();
             data.no = i+1;
             data.title = "데이터"+i;
+            result.add(data);
         }
         return result;
     }
